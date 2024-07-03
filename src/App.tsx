9900bb85390f82c7c8b7f76d28448c7b8a077465
dohmeid <doha.hmeid@gmail.com>
2024-07-03 15:29:58 +0300

    basic setup for the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <p>hello</p>
+    </div>
+  );
+};
+
+export default App;
