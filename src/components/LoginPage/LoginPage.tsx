490344b239d4b0f5c8caded3d2a54fd35fbe4d7f
dohmeid <doha.hmeid@gmail.com>
2024-07-04 19:11:22 +0300

    using Formik for handling login form state

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,32 +1,15 @@
-import React, { useState, FC } from "react";
+import React, { FC } from "react";
 import classes from "./LoginPage.module.css";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup"; //Yup for object schema validation
+
+// the yup module schema for validation
+const schema = Yup.object().shape({
+  username: Yup.string().required("Username is a required field!"),
+  password: Yup.string().required("Password is a required field!"),
+});
 
 const LoginPage: FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [isValid, setValid] = useState(false);
-
-  /*
-  const setText = event => {
-    event.target.name === "name"
-      ? setUsername(event.target.value)
-      : setPassword(event.target.value);
-  };
-
-  const validate = () => {
-    setValid(!!username && !!password);
-    if (!username) {
-      document.querySelector("#username").focus();
-    } else if (!password) {
-      document.querySelector("#password").focus();
-    }
-  };*/
-
-  const submitFormHandler = (e: any) => {
-    e.preventDefault();
-    //validate();
-  };
-
   return (
     <div className={classes.container}>
       <div className={classes.headline}>
@@ -39,68 +22,51 @@ const LoginPage: FC = () => {
         </p>
       </div>
 
-      <form
-        aria-labelledby="log-in information"
-        className={classes.loginForm}
-        onSubmit={submitFormHandler}
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={schema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
       >
-        <div className={classes.inputContainer}>
-          <label
-            htmlFor="userName"
-            className={`${classes.label} ${!isValid ? classes.error : ""}`}
+        {(formik) => (
+          <Form
+            aria-labelledby="log-in information"
+            className={classes.loginForm}
           >
-            Username
-          </label>
-          <input
-            className={`${!isValid ? classes.errorBorder : ""}`}
-            type="text"
-            name="userName"
-            id="userName"
-            aria-required="true"
-            aria-describedby="usernameAlert"
-          />
-          {!isValid && !username && (
-            <div
-              role="alert"
-              aria-atomic="true"
-              id="usernameAlert"
-              className={classes.alert}
-            >
-              You must enter a valid username
+            <div className={classes.inputContainer}>
+              <label htmlFor="username" className={classes.label}>
+                Username
+              </label>
+              <Field
+                type="text"
+                name="username"
+                aria-describedby="usernameAlert"
+              />
+              <ErrorMessage name="username" className={classes.errorAlert} />
             </div>
-          )}
-        </div>
 
-        <div className={classes.inputContainer}>
-          <label
-            htmlFor="password"
-            className={`${classes.label} ${!isValid ? classes.error : ""}`}
-          >
-            Password
-          </label>
-          <input
-            className={`${!isValid ? classes.errorBorder : ""}`}
-            type="password"
-            name="password"
-            id="password"
-            aria-required="true"
-            aria-describedby="passwordAlert"
-          />
-          {!isValid && !password && (
-            <div
-              aria-live="polite"
-              role="alert"
-              aria-atomic="true"
-              id="passwordAlert"
-              className={classes.alert}
-            >
-              You must enter a valid password
+            <div className={classes.inputContainer}>
+              <label htmlFor="password" className={classes.label}>
+                Password
+              </label>
+              <Field
+                type="password"
+                name="password"
+                aria-describedby="passwordAlert"
+              />
+              <ErrorMessage name="password" className={classes.errorAlert} />
             </div>
-          )}
-        </div>
 
-        <button type="submit">Login</button>
-      </form>
+            <button type="submit" disabled={formik.isSubmitting}>
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
