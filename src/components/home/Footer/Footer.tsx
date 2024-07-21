b35cc4b145ce51ded7a624aaf9691202a5b0f598
dohmeid <doha.hmeid@gmail.com>
2024-07-21 17:20:53 +0300

    creating the home page footer

--- src/components/home/Footer/Footer.tsx
+++ src/components/home/Footer/Footer.tsx
@@ -3,9 +3,42 @@ import classes from "./Footer.module.css";
 
 const Footer: FC = () => {
   return (
-    <>
-      <p>Footer</p>
-    </>
+    <div className={classes.footer}>
+      <div className={classes.copyrightContainer}>
+        <p>Copyright @ 2024 TravelHub.</p>
+        <p>Designed and Developed by Doha Hmeid. All Rights Reserved.</p>
+      </div>
+
+      <ul className={classes.socialLinks}>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/dohmeid/"
+            target="_blank"
+            aria-label="Go to LinkedIn"
+          >
+            <i className="bi bi-linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/dohmeid"
+            target="_blank"
+            aria-label="Go to GitHub"
+          >
+            <i className="bi bi-github"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://m.facebook.com/doha.hmeid?mibextid=LQQJ4d"
+            target="_blank"
+            aria-label="Go to Facebook"
+          >
+            <i className="bi bi-facebook"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
