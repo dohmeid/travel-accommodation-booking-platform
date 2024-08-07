b8546c4bab764f711508bb2256578823c1b55568
dohmeid <doha.hmeid@gmail.com>
2024-08-07 17:12:22 +0300

    moving Destination Card to separate component + making them responsive

---
+++ src/components/home/TrendingDestinations/DestinationCard/DestinationCard.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import { Destination } from "../../../../interfaces/homePageTypes";
+import classes from "./DestinationCard.module.css";
+
+interface Props {
+  city: Destination;
+}
+
+const DestinationCard: FC<Props> = ({ city }) => {
+  return (
+    <div className={classes.card} key={city.cityId}>
+      <img src={city.thumbnailUrl} alt={`${city.cityName} thumbnail`} />
+      <p className={classes.name}>
+        {city.cityName}, <span>{city.countryName}</span>
+      </p>
+      <div className={classes.description}>{city.description}</div>
+    </div>
+  );
+};
+
+export default DestinationCard;
--- src/components/home/TrendingDestinations/TrendingDestinations.tsx
+++ src/components/home/TrendingDestinations/TrendingDestinations.tsx
@@ -1,32 +1,26 @@
 import React, { FC } from "react";
+import { useHomeContext } from "../../../context/homeProvider";
+import DestinationCard from "./DestinationCard/DestinationCard";
 import classes from "./TrendingDestinations.module.css";
-import { useHomeProvider } from "../../../context/homeProvider";
 
 const TrendingDestinations: FC = () => {
-  const { destinations } = useHomeProvider();
+  const { destinations } = useHomeContext();
+
+  //rendering the destinations list
+  const DESTINATIONS = destinations.map((item) => (
+    <DestinationCard key={item.cityId} city={item} />
+  ));
 
   return (
     <div className={classes.trendingDestinationsSection}>
       <h2>Trending Destinations</h2>
-      <p>
+      <h3>
         Planning you next adventure? Here are the most popular choices for
         travellers.
-      </p>
+      </h3>
 
       <div className={classes.list}>
-        {destinations.length === 0 ? (
-          <p>Loading...</p>
-        ) : (
-          destinations.map((city) => (
-            <div className={classes.item} key={city.cityId}>
-              <img src={city.thumbnailUrl} alt={`${city.cityName} thumbnail`} />
-              <p className={classes.name}>
-                {city.cityName}, <span>{city.countryName}</span>
-              </p>
-              <div className={classes.description}>{city.description}</div>
-            </div>
-          ))
-        )}
+        {destinations.length === 0 ? <p>Loading...</p> : DESTINATIONS}
       </div>
     </div>
   );
