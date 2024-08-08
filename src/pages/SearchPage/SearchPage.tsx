50f5a2162361c028d68942a367f78217e0a4e908
dohmeid <doha.hmeid@gmail.com>
2024-08-08 23:26:26 +0300

    changing the layout of search page and making it responsive

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,15 +1,15 @@
-import React, { useState, FC } from "react";
-import classes from "./SearchPage.module.css";
+import React, { FC } from "react";
 import SearchBar from "../../components/search/HotelsSearchBar/HotelsSearchBar";
 import Filters from "../../components/search/Filters/Filters";
 import Results from "../../components/search/Results/Results";
+import classes from "./SearchPage.module.css";
 
 const SearchPage: FC = () => {
   return (
     <div className={classes.searchPage}>
-      <SearchBar />
-      <div className={classes.flexContainer}>
-        <Filters />
+      <Filters />
+      <div className={classes.flex}>
+        <SearchBar />
         <Results />
       </div>
     </div>
