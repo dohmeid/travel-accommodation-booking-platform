fd804a97f2dc813a88f8c7331ba33da3a61ec26d
dohmeid <doha.hmeid@gmail.com>
2024-08-11 01:22:59 +0300

    Reorder context providers in UserPage component

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,28 +1,28 @@
 import React, { FC } from "react";
+import { Outlet } from "react-router-dom";
+import { SearchProvider } from "../../context/searchProvider";
+import { CartProvider } from "../../context/cartProvider";
+import { HomeProvider } from "../../context/homeProvider";
+import { HotelProvider } from "../../context/hotelProvider";
 import MainHeader from "../../components/common/MainHeader/MainHeader";
 import Footer from "../../components/common/Footer/Footer";
 import classes from "./UserPage.module.css";
-import { Outlet } from "react-router-dom";
-import { SearchProvider } from "../../context/searchProvider";
-import { HotelProvider } from "../../context/hotelProvider";
-import { CartProvider } from "../../context/cartProvider";
-import { HomeProvider } from "../../context/homeProvider";
 
 const UserPage: FC = () => {
   return (
-    <SearchProvider>
-      <HotelProvider>
-        <CartProvider>
-          <HomeProvider>
+    <CartProvider>
+      <HomeProvider>
+        <SearchProvider>
+          <HotelProvider>
             <div className={classes.userPage}>
               <MainHeader />
               <Outlet />
               <Footer />
             </div>
-          </HomeProvider>
-        </CartProvider>
-      </HotelProvider>
-    </SearchProvider>
+          </HotelProvider>
+        </SearchProvider>
+      </HomeProvider>
+    </CartProvider>
   );
 };
 
