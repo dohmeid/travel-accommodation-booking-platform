53a7325e595bb73d51b677f25e74ba337ac3c2da
dohmeid <doha.hmeid@gmail.com>
2024-08-20 13:13:46 +0300

    Add unit tests for login form

---
+++ src/__tests__/components/render.tsx
@@ -0,0 +1,29 @@
+import React, { FC, ReactNode } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from '../../context/authProvider';
+import { NotificationProvider } from '../../context/NotificationProvider';
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+const Wrapper: FC<WrapperProps> = ({ children }) => {
+  return (
+    <NotificationProvider>
+      <BrowserRouter>
+        <AuthProvider>{children}</AuthProvider>
+      </BrowserRouter>
+    </NotificationProvider>
+  );
+};
+
+const testRender = (
+  Component: React.ReactElement,
+  options?: RenderOptions,
+) => {
+  return render(Component, { wrapper: Wrapper, ...options });
+};
+
+export { testRender as render };
+
