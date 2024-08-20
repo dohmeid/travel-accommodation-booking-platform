8335bc9f2909dd911db06e5727f9447b1d17d9e4
dohmeid <doha.hmeid@gmail.com>
2024-08-20 13:15:53 +0300

    Add ErrorBoundary to catch unexpected errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC, lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from './routes/PrivateRoute';
+import MyErrorBoundary from './components/shared/ErrorBoundary/ErrorBoundary';
 import CursorFollower from './components/shared/CursorFollower/CursorFollower';
 import Spinner from './components/shared/Spinner/Spinner';
 
@@ -28,45 +29,50 @@ const App: FC = () => {
     <>
       <CursorFollower />
 
-      <Suspense fallback={<Spinner />}>
-        <Routes>
-          <Route index element={<LoginPage />} />
-          <Route path="login" element={<LoginPage />} />
+      <MyErrorBoundary>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route index element={<LoginPage />} />
+            <Route path="login" element={<LoginPage />} />
 
-          <Route
-            path="main"
-            element={
-              <PrivateRoute allowedRoles={['User']}>
-                <UserPage />
-              </PrivateRoute>
-            }
-          >
-            <Route index element={<HomePage />} />
-            <Route path="home" element={<HomePage />} />
-            <Route path="search" element={<SearchPage />} />
-            <Route path="checkout" element={<CheckoutPage />} />
-            <Route path="confirmation" element={<ConfirmationPage />} />
-            <Route path="hotel/:hotelId" element={<HotelPage />} />
-          </Route>
+            <Route
+              path="main"
+              element={
+                <PrivateRoute allowedRoles={['User']}>
+                  <UserPage />
+                </PrivateRoute>
+              }
+            >
+              <Route index element={<HomePage />} />
+              <Route path="home" element={<HomePage />} />
+              <Route path="search" element={<SearchPage />} />
+              <Route path="checkout" element={<CheckoutPage />} />
+              <Route path="confirmation" element={<ConfirmationPage />} />
+              <Route path="hotel/:hotelId" element={<HotelPage />} />
+            </Route>
 
-          <Route
-            path="adminPortal"
-            element={
-              <PrivateRoute allowedRoles={['Admin']}>
-                <AdminPage />
-              </PrivateRoute>
-            }
-          >
-            <Route index element={<Grid gridType="city" />} />
-            <Route path="city" element={<Grid gridType="city" />} />
-            <Route path="hotel" element={<Grid gridType="hotel" />} />
-            <Route path="room" element={<Grid gridType="city" />} />
-          </Route>
+            <Route
+              path="adminPortal"
+              element={
+                <PrivateRoute allowedRoles={['Admin']}>
+                  <AdminPage />
+                </PrivateRoute>
+              }
+            >
+              <Route index element={<Grid gridType="city" />} />
+              <Route path="city" element={<Grid gridType="city" />} />
+              <Route path="hotel" element={<Grid gridType="hotel" />} />
+              <Route path="room" element={<Grid gridType="city" />} />
+            </Route>
 
-          <Route path="unauthorizedUser" element={<UnauthorizedErrorPage />} />
-          <Route path="*" element={<NotFoundErrorPage />} />
-        </Routes>
-      </Suspense>
+            <Route
+              path="unauthorizedUser"
+              element={<UnauthorizedErrorPage />}
+            />
+            <Route path="*" element={<NotFoundErrorPage />} />
+          </Routes>
+        </Suspense>
+      </MyErrorBoundary>
     </>
   );
 };
